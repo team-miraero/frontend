8f475716fbf0a2b456b83b8552a7a4aa9f1d65a3
Mr.Song <lssyl_@naver.com>
2026-07-24 17:31:54 +0900

    [Feat] AI 목표 코치 채팅 UI 구현 (#7)
    * feat: AI 목표 코치 기본 UI 구현 & Mock기반 API 호출 구조 설계
    
    * feat: AI 목표 코치 기본 헤더 숨기기
    
    * chore: lint파일 src코드만 검사하도록 변경

--- src/app/layouts/DashboardLayout.vue
+++ src/app/layouts/DashboardLayout.vue
@@ -1,10 +1,10 @@
 <!-- 좌측 사이드바 + 헤더 + 컨텐츠 영역 레이아웃 -->
 <template>
-  <div class="flex min-h-screen w-full bg-[#f8fbff]">
+  <div class="flex h-screen w-full overflow-hidden bg-[#f8fbff]">
     <DashboardSidebar />
-    <div class="flex flex-1 flex-col">
-      <DashboardHeader />
-      <main class="flex-1">
+    <div class="flex min-w-0 flex-1 flex-col">
+      <DashboardHeader v-if="!route.meta.hideHeader" />
+      <main class="flex-1 overflow-y-auto">
         <RouterView />
       </main>
     </div>
@@ -12,7 +12,10 @@
 </template>
 
 <script setup>
+import { useRoute } from 'vue-router'
 import { RouterView } from 'vue-router'
 import DashboardHeader from '@/app/layouts/components/DashboardHeader.vue'
 import DashboardSidebar from './components/DashboardSidebar.vue'
+
+const route = useRoute()
 </script>
