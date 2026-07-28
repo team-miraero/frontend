47aaa4218732f0272cfa527249a976b4a551b5b2
jiwon <jiwonkimz12@gmail.com>
2026-07-28 15:50:56 +0900

    refactor: 지출 페이지 feature 진입 구조 정리

--- src/pages/spending/SpendingPage.vue
+++ src/pages/spending/SpendingPage.vue
@@ -1,13 +1,7 @@
 <template>
-  <main>
-    <!-- 페이지 상단 영역 -->
-
-    <SpendingSummarySection />
-
-    <!-- 절감 시뮬레이터 -->
-  </main>
+  <SpendingDashboard />
 </template>
 
 <script setup>
-import SpendingSummarySection from '@/features/spending/components/SpendingSummarySection.vue'
+import { SpendingDashboard } from '@/features/spending'
 </script>
