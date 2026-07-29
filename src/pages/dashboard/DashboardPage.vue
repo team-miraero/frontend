c2bdd9f8583e4c98addd23f84b0f8cde89203cd7
SongCodeMaster <lssyl_@naver.com>
2026-07-29 11:20:27 +0900

    feat: 로드맵 대시보드 반응형 대시보드 구현

--- src/pages/dashboard/DashboardPage.vue
+++ src/pages/dashboard/DashboardPage.vue
@@ -6,18 +6,58 @@
         :pace="goalStore.currentGoal.pace"
         :progress-rate="goalStore.currentGoal.progressRate"
       />
+      <!-- ============ 데스크톱 (lg 이상): 기존 3열 + 사이드 패널 구조 유지 ============ -->
+      <div class="hidden lg:block">
+        <div class="grid grid-cols-3 gap-4 pt-6">
+          <GoalSummaryCard :goal="goalStore.currentGoal" />
+          <ConnectedAssetsCard :assets="goalStore.assets" />
+          <PacemakerToggleCard
+            :pacemaker="pacemakerStore.pacemakerStatus"
+            @toggle="pacemakerStore.togglePacemaker"
+          />
+        </div>
 
-      <div class="grid grid-cols-3 gap-4 pt-6">
-        <GoalSummaryCard :goal="goalStore.currentGoal" />
-        <ConnectedAssetsCard :assets="goalStore.assets" />
-        <PacemakerToggleCard
-          :pacemaker="pacemakerStore.pacemakerStatus"
-          @toggle="pacemakerStore.togglePacemaker"
-        />
+        <div class="flex gap-5 pt-6">
+          <div class="flex flex-1 min-w-0 flex-col gap-4">
+            <MilestoneProgressBar
+              :goal="goalStore.currentGoal"
+              :milestones="roadmapStore.milestones"
+              @pause="goalStore.updateCurrentGoalStatus('PAUSE')"
+            />
+            <NextMilestoneCard
+              :goal="goalStore.currentGoal"
+              :milestones="roadmapStore.milestones"
+            />
+            <MilestoneList :milestones="roadmapStore.milestones" />
+          </div>
+          <div class="w-[300px] shrink-0">
+            <AvailableMoneyPanel
+              v-if="goalStore.availableMoney"
+              :available-money="goalStore.availableMoney"
+            />
+          </div>
+        </div>
       </div>
 
-      <div class="flex gap-5 pt-6">
-        <div class="flex flex-1 min-w-0 flex-col gap-4">
+      <!-- ============ 모바일 (lg 미만): 2열 페어 그리드로 재배치 ============ -->
+      <div class="lg:hidden">
+        <div class="grid grid-cols-2 gap-4 pt-6">
+          <GoalSummaryCard :goal="goalStore.currentGoal" />
+          <ConnectedAssetsCard :assets="goalStore.assets" />
+        </div>
+
+        <div class="grid grid-cols-2 gap-4 pt-4">
+          <AvailableMoneyPanel
+            v-if="goalStore.availableMoney"
+            :available-money="goalStore.availableMoney"
+          />
+          <PacemakerToggleCard
+            :pacemaker="pacemakerStore.pacemakerStatus"
+            @toggle="pacemakerStore.togglePacemaker"
+          />
+        </div>
+
+        <div class="flex flex-col gap-4 pt-4">
           <MilestoneProgressBar
             :goal="goalStore.currentGoal"
             :milestones="roadmapStore.milestones"
@@ -26,10 +66,6 @@
           <NextMilestoneCard :goal="goalStore.currentGoal" :milestones="roadmapStore.milestones" />
           <MilestoneList :milestones="roadmapStore.milestones" />
         </div>
-        <AvailableMoneyPanel
-          v-if="goalStore.availableMoney"
-          :available-money="goalStore.availableMoney"
-        />
       </div>
     </div>
   </div>
@@ -52,7 +88,6 @@ import {
 } from '@/features/roadmap'
 import { usePacemakerStore } from '@/features/pacemaker'
 import * as goalApi from '@/features/goal/api/goal.api'
-
 const route = useRoute()
 const goalStore = useGoalStore()
 const roadmapStore = useRoadmapStore()
