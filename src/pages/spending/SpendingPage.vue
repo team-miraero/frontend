341cc1e2b573155d495b97e955e350d50fa47bb2
kamzajjang <jiwonkimz12@gmail.com>
2026-07-29 18:48:02 +0900

    feat: 지출관리·KB상품 로드맵 선택 컴포넌트 구현 (#19)

--- src/pages/spending/SpendingPage.vue
+++ src/pages/spending/SpendingPage.vue
@@ -1,7 +1,24 @@
 <template>
-  <SpendingDashboard />
+  <SpendingDashboard
+    :goals="goals"
+    :selected-goal-id="selectedGoalId"
+    :selected-goal="selectedGoalName"
+    :are-goals-loading="areGoalsLoading"
+    @select-goal="goalStore.selectGoal"
+  />
 </template>
 
 <script setup>
+import { computed, onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
 import { SpendingDashboard } from '@/features/spending'
+import { useGoalStore } from '@/features/goal'
+
+const goalStore = useGoalStore()
+const { goals, selectedGoalId, selectedGoal, areGoalsLoading } = storeToRefs(goalStore)
+const selectedGoalName = computed(() => selectedGoal.value?.goalName ?? '')
+
+onMounted(() => {
+  goalStore.fetchGoals()
+})
 </script>
