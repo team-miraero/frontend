93dabc770a05ad0dfc853d0b582700fe797f66f4
mantalgap <wjbok02@naver.com>
2026-07-30 17:41:00 +0900

    fix/라우팅 코드 수정

--- src/features/goal/routes.js
+++ src/features/goal/routes.js
@@ -4,29 +4,23 @@ import GoalLayout from '@/app/layouts/GoalLayout.vue'
 
 export const goalRoutes = [
   {
-    path: '/goal',
-    component: GoalLayout,
-    children: [
-      {
-        path: 'select',
-        name: ROUTE_NAMES.GOAL_SELECT,
-        component: () => import('@/pages/goal/GoalSelectPage.vue'),
-      },
-      {
-        path: 'detail',
-        name: ROUTE_NAMES.GOAL_DETAIL,
-        component: () => import('@/pages/goal/GoalDetailPage.vue'),
-      },
-      {
-        path: 'feasibility',
-        name: ROUTE_NAMES.GOAL_FEASIBILITY,
-        component: () => import('@/pages/goal/FeasibilityPage.vue'),
-      },
-      {
-        path: 'account',
-        name: ROUTE_NAMES.GOAL_ACCOUNT,
-        component: () => import('@/pages/goal/AccountLinkPage.vue'),
-      },
-    ],
+    path: '/goal/select',
+    name: ROUTE_NAMES.GOAL_SELECT,
+    component: () => import('@/pages/goal/GoalSelectPage.vue'),
+  },
+  {
+    path: '/goal/detail',
+    name: ROUTE_NAMES.GOAL_DETAIL,
+    component: () => import('@/pages/goal/GoalDetailPage.vue'),
+  },
+  {
+    path: '/goal/feasibility',
+    name: ROUTE_NAMES.GOAL_FEASIBILITY,
+    component: () => import('@/pages/goal/FeasibilityPage.vue'),
+  },
+  {
+    path: '/goal/account',
+    name: ROUTE_NAMES.GOAL_ACCOUNT,
+    component: () => import('@/pages/goal/AccountLinkPage.vue'),
   },
 ]
