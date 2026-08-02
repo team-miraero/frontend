3625168cef3ad3d976488f7743e3753b43f980f6
jiwon <jiwonkimz12@gmail.com>
2026-08-02 18:18:05 +0900

    feat: 예적금 상품 조회 API와 목업 추가

--- src/mocks/handlers/index.js
+++ src/mocks/handlers/index.js
@@ -3,5 +3,12 @@ import { authHandlers } from '@/mocks/handlers/auth'
 import { goalHandlers } from '@/mocks/handlers/goal'
 import { pacemakerHandlers } from '@/mocks/handlers/pacemaker'
 import { coachHandlers } from '@/mocks/handlers/coach'
+import { productsHandlers } from '@/mocks/handlers/products'
 
-export const handlers = [...authHandlers, ...goalHandlers, ...pacemakerHandlers, ...coachHandlers]
+export const handlers = [
+  ...authHandlers,
+  ...goalHandlers,
+  ...pacemakerHandlers,
+  ...coachHandlers,
+  ...productsHandlers,
+]
