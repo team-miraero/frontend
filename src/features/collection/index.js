ed02522758f3d0f16e868c4a68d94e97a5e1bc95
mantalgap <109576088+mantalgap@users.noreply.github.com>
2026-08-07 11:40:14 +0900

    [Feat] 목표컬렉션 대시보드 UI 구현 (#66)
    * feat: 목표 컬렉션 페이지 및 컴포넌트 구현
    
    * refactor: 불필요한 CollectionHeader 컴포넌트 삭제 및 컬렉션 페이지 UI 구조 정리
    
    * 로드맵 마일스톤 컴포넌트(MilestoneProgressBar, NextMilestoneCard) 코드 원격 코드로 수정

--- src/features/collection/index.js
+++ src/features/collection/index.js
@@ -2,3 +2,7 @@
 export { collectionRoutes } from '@/features/collection/routes'
 export { useCollectionStore } from '@/features/collection/store/collection.store'
 export * as collectionApi from '@/features/collection/api/collection.api'
+
+export { default as EmptyCollection } from '@/features/collection/components/EmptyCollection.vue'
+export { default as CollectionCard } from '@/features/collection/components/CollectionCard.vue'
+export { default as NextGoalBanner } from '@/features/collection/components/NextGoalBanner.vue'
--- src/features/collection/store/collection.store.js
+++ src/features/collection/store/collection.store.js
@@ -1,10 +1,48 @@
 // collection 도메인 상태 store
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import * as collectionApi from '@/features/collection/api/collection.api'
 
 export const useCollectionStore = defineStore('feature-collection', () => {
-  // 담을 상태: collectionStatus
   const collectionStatus = ref(null)
 
-  return { collectionStatus }
+  /** @type {import('vue').Ref<import('@/features/collection/api/collection.api').AchievedGoal[]>} */
+  const achievedGoals = ref([])
+  const isLoading = ref(false)
+  const error = ref(null)
+
+  const hasGoals = computed(
+    () => Array.isArray(achievedGoals.value) && achievedGoals.value.length > 0
+  )
+
+  async function fetchAchievedGoals() {
+    isLoading.value = true
+    error.value = null
+    try {
+      const data = await collectionApi.getAchievedGoals()
+      achievedGoals.value = Array.isArray(data) ? data : []
+    } catch (caughtError) {
+      error.value = caughtError
+      achievedGoals.value = []
+    } finally {
+      isLoading.value = false
+    }
+  }
+
+  /**
+   * 테스트/시연용: 빈 상태로 토글하는 함수
+   */
+  function clearAchievedGoals() {
+    achievedGoals.value = []
+  }
+
+  return {
+    collectionStatus,
+    achievedGoals,
+    isLoading,
+    error,
+    hasGoals,
+    fetchAchievedGoals,
+    clearAchievedGoals,
+  }
 })
